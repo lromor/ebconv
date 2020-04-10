70214b028117e936b50f4b8aadc95807a7a64bec
Leonardo Romor <leonardo.romor@gmail.com>
2020-04-11 01:57:19 +0200

    Removing translate function from functional.

--- src/ebconv/nn/functional.py
+++ src/ebconv/nn/functional.py
@@ -372,38 +372,3 @@ def crop(input_: torch.Tensor, crop_: Tuple[int, ...]) -> torch.Tensor:
 
     # Apply the crop and return
     return input_[slices]
-
-
-def translate(input_: torch.Tensor, shift: Tuple[int, ...],
-              mode='constant', value=0) -> torch.Tensor:
-    """Translate the input.
-
-    Args:
-        x: Input tensor
-        shift: Represents the shift values for each spatial axis.
-        mode: Same argument as torch.nn.functional.pad
-        value: Same as above.
-
-    Returns:
-        Translated tensor.
-
-    """
-    # Construct the bounds and padding list of tuples
-    paddings = []
-    slices = [...]
-    for axis_shift in reversed(shift):
-        abs_shift = abs(axis_shift)
-        if axis_shift > 0:
-            paddings.append(abs_shift)
-            paddings.append(0)
-            slices.insert(1, slice(0, -abs_shift if abs_shift else None))
-        else:
-            paddings.append(0)
-            paddings.append(abs_shift)
-            slices.insert(1, slice(abs_shift, None))
-
-    slices = tuple(slices)
-    output = torch.nn.functional.pad(input_, paddings, mode=mode, value=value)
-
-    # Apply the crop and return
-    return output[slices]
