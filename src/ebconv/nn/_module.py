Fixed module not correctly changing gradient flag when using adaptive_scalings/adaptive_centers

src/ebconv/nn/_module.py
@@ -104,18 +104,18 @@ class CBSConv(torch.nn.Module):
     def reset_parameters(self):
         """Reset parameters using default initializers."""
         if self.layout == 'grid':
-            self.centers = Parameter(torch.stack([
+            self.centers.data = torch.stack([
                 torch.from_numpy(
                     create_uniform_grid(self.kernel_size)).float()
-                for _ in range(self.basis_groups)]))
+                for _ in range(self.basis_groups)])
         else:
-            self.centers = Parameter(torch.stack([
+            self.centers.data = torch.stack([
                 torch.from_numpy(
                     create_random_centers(self.kernel_size, self.nc)).float()
-                for _ in range(self.basis_groups)]))
+                for _ in range(self.basis_groups)])
 
-        self.scalings.data = Parameter(
-            torch.ones(self.basis_groups, self.nc, self.dims))
+        self.scalings.data = torch.ones(
+            self.basis_groups, self.nc, self.dims)
 
         init.kaiming_uniform_(self.weights, a=math.sqrt(5))
         if self.bias is not None:

tests/nn/test_module.py
@@ -7,23 +7,47 @@ import torch
 from ebconv.nn import CBSConv
 
 
+@pytest.mark.parametrize('adaptive_centers', [True, False])
+@pytest.mark.parametrize('adaptive_scalings', [True, False])
 @pytest.mark.parametrize('k', [0, 1, 2, 3])
-def test_cbconv_module(k):
-    """Check basic functionality of the module.
+@pytest.mark.parametrize('nc', [None, (20)])
+@pytest.mark.parametrize('layout', ['grid', 'random'])
+def test_cbconv_module(layout, nc, k, adaptive_centers, adaptive_scalings):
+    """Check basic functionality of the module."""
+    input_channels = 3
+    output_channels = 4
 
-    By default torch modules run with 32-bit floating point weights.
-    """
-    input_ = torch.rand(1, 3, 256, 256)
+    input_ = torch.rand(1, input_channels, 256, 256)
+
+    def create_module():
+        return CBSConv(
+            input_channels, output_channels, (20, 20),
+            layout=layout, nc=nc, k=k,
+            adaptive_centers=adaptive_centers,
+            adaptive_scalings=adaptive_scalings)
 
     if k < 1:
         with pytest.raises(ValueError):
-            module = CBSConv(
-                3, 1, (20, 20), 'random', 8, k, padding=2, bias=True)
+            module = create_module()
         return
-    module = CBSConv(3, 1, (20, 20), 'random', 8, k, padding=2, bias=True)
 
+    if layout == 'grid' and nc is not None:
+        with pytest.warns(UserWarning):
+            module = create_module()
+    elif layout == 'random' and nc is None:
+        with pytest.raises(ValueError):
+            module = create_module()
+        return
+    else:
+        module = create_module()
+
+    # Check by default they are all of the same type (float 32)
     for param in module.parameters():
         assert param.dtype == torch.float
+
+    assert module.centers.requires_grad == adaptive_centers
+    assert module.scalings.requires_grad == adaptive_scalings
+    assert module.weights.requires_grad
 
     y = module(input_)
     assert y.dtype == torch.float