fd8259068cff689635fa307933999624483a073c
Leonardo Romor <leonardo.romor@gmail.com>
2020-06-08 18:02:09 +0200

    Added CBSConv2d test that ensures the results are identical in the case with k=0 and a standard convolution.

--- tests/nn/test_module.py
+++ tests/nn/test_module.py
@@ -5,6 +5,7 @@ import pytest
 import torch
 
 from ebconv.nn import CBSConv
+from ebconv.nn import CBSConv2d
 
 
 @pytest.mark.parametrize('adaptive_centers', [True, False])
@@ -57,3 +58,59 @@ def test_cbconv_module(layout, nc, k, adaptive_centers, adaptive_scalings):
 
     assert y.dtype == torch.float
     assert y.requires_grad
+
+
+@pytest.mark.parametrize('dilation', [1, 2])
+@pytest.mark.parametrize('padding', [1, 3])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('stride', [1, 2, 3])
+@pytest.mark.parametrize('kernel_size', [3, 7])
+@pytest.mark.parametrize('separable', [True, False])
+def test_cbsconv_vs_standard(separable, kernel_size, stride, bias, padding, dilation):
+    """Test that cbsconv can emulate standard conv."""
+    batch = 2
+    input_channels = 4
+    output_channels = 8
+    input_size = (32, 48)
+
+    input_ = torch.rand(batch, input_channels, *input_size)
+    standard_layer = torch.nn.Conv2d(
+        input_channels, output_channels, kernel_size=kernel_size,
+        stride=stride, bias=bias, padding=padding, dilation=dilation)
+
+    cbsconv_layer = CBSConv2d(
+        input_channels, output_channels, kernel_size=kernel_size,
+        stride=stride, bias=bias, k=0, padding=padding,
+        dilation=dilation, layout='grid', adaptive_centers=False,
+        adaptive_scalings=False, separable=separable)
+
+    loss = torch.nn.MSELoss()
+
+    weights = torch.empty_like(standard_layer.weight)
+    weights.requires_grad = True
+    torch.nn.init.kaiming_normal_(weights, mode='fan_out', nonlinearity='relu')
+
+
+    with torch.no_grad():
+        standard_layer.weight.data = weights
+        cbsconv_layer.weights.data = weights.reshape(
+            *cbsconv_layer.weights.data.shape)
+        cbsconv_layer.bias = standard_layer.bias
+
+    assert torch.allclose(
+        standard_layer.weight.view(-1), cbsconv_layer.weights.view(-1))
+    result_standard = standard_layer(input_)
+    result_cbsconv = cbsconv_layer(input_)
+
+    target = torch.zeros_like(result_standard)
+    ls_out = loss(result_standard, target)
+    lc_out = loss(result_cbsconv, target)
+
+    ls_out.backward()
+    lc_out.backward()
+
+    assert torch.allclose(result_standard, result_cbsconv, atol=1e-5)
+    assert torch.allclose(
+        standard_layer.weight.grad.view(-1),
+        cbsconv_layer.weights.grad.view(-1),
+        atol=1e-5)
